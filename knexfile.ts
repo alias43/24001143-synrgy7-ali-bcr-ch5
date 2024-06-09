82a13953c80c9ce823feb5e23c514e433742ab41
alias43 <aliridho43@gmail.com>
2024-06-09 22:52:49 +0700

    init commit

---
+++ knexfile.ts
@@ -0,0 +1,39 @@
+import type { Knex } from "knex";
+
+// Update with your config settings.
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: "db_cars_orm",
+      user: "postgres",
+      password: "15052024",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "db_cars_orm",
+      user: "postgres",
+      password: "15052024",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
+
+module.exports = config;
